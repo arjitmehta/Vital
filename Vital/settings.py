41c79efa300224fa361de04bda7ebaf9e9043c4e
Arjit Mehta <arjit.mehta@enumeracy.org>
2024-07-03 21:53:14 +0530

    Added Local host in the allowed hosts.

--- Vital/settings.py
+++ Vital/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e-5(_6p=u1+alh4xnp2#ze7$sgwvq9emg3e16-^^ai4j-)nl=l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['vital-4zsb.onrender.com']
+ALLOWED_HOSTS = ['vital-4zsb.onrender.com','127.0.0.1']
 
 
 # Application definition
