0249765ac01609158437a21241d8472e8f1091fb
Arjit Mehta <arjit.mehta@enumeracy.org>
2025-02-04 19:16:28 +0530

    Capture the live stream from frontend and push each frame to backend for computer vision Laggy but working.

--- vital_ai/urls.py
+++ vital_ai/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('', views.index, name='index'),
+#     path('<str:aasan>/video_feed/', views.video_feed, name='video_feed'),
+#     path('<str:aasan>/', views.index, name='aasan'),
+# 
+
+from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:aasan>/video_feed/', views.video_feed, name='video_feed'),
-    path('<str:aasan>/', views.index, name='aasan'),
+    path('video_feed/', views.video_feed, name='video_feed'),
 ]
--- vital_ai/views.py
+++ vital_ai/views.py
@@ -1,161 +1,118 @@
+
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.http import StreamingHttpResponse
 import cv2
 import numpy as np
-import os
-import random
 from .PoseModule import poseDetector
 from .Activity import activity
 
-
-
-def index(request,aasan='inter_1'):
+def index(request, aasan='inter_1'):
     print(aasan)
     context = {
         'aasan': aasan
     }
-    return render(request, 'index.html',context)
+    return render(request, 'index.html', context)
 
-def video_stream(aasan='inter_1'):
-    # a="inter_1"
-    # a="inter_"+str(random.randrange(1,11))
-    a=aasan
-    # print('this---',act,a)
-    b=0
-    ex=0
-    cap = cv2.VideoCapture(cv2.CAP_ANY)
-    
-
-    # VITAL = activity()
+def video_stream(frame, aasan='inter_1'):
+    # Flip the frame and process it via OpenCV and computer vision methods.
+    processed_frame = cv2.flip(frame, 1)
     detector = poseDetector()
-    count = 0
-    dir = 0
-    per=0
-    pTime = 0
-    while True:
-        success, processed_frame = cap.read()
-        if not success:
-            break
-
-        #processed_frame = cv2.imread("Yoga_test_images/Beginner/beg_6.png")
-        #processed_frame = cv2.resize(processed_frame, (720, 720))
-        processed_frame = cv2.flip(processed_frame, 1)
-        processed_frame = detector.findPose(processed_frame,False)
-        lmList = detector.findPosition(processed_frame, False)
-        # print(lmList)
-        if len(lmList) != 0:
-            
-            if a== 'ex_1' :
-                per = activity.lifting_curls_biceps(processed_frame,detector)
-                ex=1
-            if a== 'ex_2' :
-                per = activity.lifting_floor_press(processed_frame,detector) #make new conditions for respective functions
-                ex=1
-            if a== 'ex_3' :
-                per = activity.lifting_bentover_dumbbell(processed_frame,detector)
-                ex=1
-            if a== 'ex_4' :
-                per = activity.lifting_forearms(processed_frame,detector)
-                ex=1
-            if a== 'ex_5' :
-                per = activity.lifting_dumbell_squats(processed_frame,detector)
-                ex=1
-            if a== 'ex_6' :
-                per = activity.lifting_shoulder_press(processed_frame,detector)
-                ex=1
-            if a =='beg_1':
-                activity.yoga_beg_aasana1(processed_frame,detector)
-            if a =='beg_2':
-                activity.yoga_beg_aasana2(processed_frame,detector)
-            if a =='beg_3':
-                activity.yoga_beg_aasana3(processed_frame,detector)
-            if a =='beg_4':
-                activity.yoga_beg_aasana4(processed_frame,detector)
-            if a =='beg_5':
-                activity.yoga_beg_aasana5(processed_frame,detector)
-            if a =='beg_6':
-                activity.yoga_beg_aasana6(processed_frame,detector)
-            if a =='beg_7':
-                activity.yoga_beg_aasana7(processed_frame,detector)
-            if a =='beg_8':
-                activity.yoga_beg_aasana8(processed_frame,detector)
-            if a =='beg_9':
-                activity.yoga_beg_aasana9(processed_frame,detector)
-            if a =='beg_10':
-                activity.yoga_beg_aasana10(processed_frame,detector)
-            if a =='inter_1':
-                activity.yoga_inter_aasana1(processed_frame,detector)
-            if a =='inter_2':
-                activity.yoga_inter_aasana2 (processed_frame,detector)
-            if a =='inter_3':
-                activity.yoga_inter_aasana3(processed_frame,detector)
-            if a =='inter_4':
-                activity.yoga_inter_aasana4(processed_frame,detector)
-            if a =='inter_5':
-                activity.yoga_inter_aasana5(processed_frame,detector)
-            if a =='inter_6':
-                activity.yoga_inter_aasana6(processed_frame,detector)
-            if a =='inter_7':
-                activity.yoga_inter_aasana7(processed_frame,detector)
-            if a =='inter_8':
-                activity.yoga_inter_aasana8(processed_frame,detector)
-            if a =='inter_9':
-                activity.yoga_inter_aasana9(processed_frame,detector)
-            if a =='inter_10':
-                activity.yoga_inter_aasana10(processed_frame,detector)
-            if a =='adv_1':
-                activity.yoga_adv_aasana1(processed_frame,detector)
-            if a =='adv_2':
-                activity.yoga_adv_aasana2(processed_frame,detector)
-            if a =='adv_3':
-                activity.yoga_adv_aasana3(processed_frame,detector)
-            if a =='adv_4':
-                activity.yoga_adv_aasana4(processed_frame,detector)
-            if a =='adv_5':
-                activity.yoga_adv_aasana5(processed_frame,detector)
-            if a =='adv_6':
-                activity.yoga_adv_aasana6(processed_frame,detector)
-            if a =='adv_7':
-                activity.yoga_adv_aasana7(processed_frame,detector)
-            if a =='adv_8':
-                activity.yoga_adv_aasana8(processed_frame,detector)
-            if a =='adv_9':
-                activity.yoga_adv_aasana9(processed_frame,detector)
-            if a =='adv_10':
-                activity.yoga_adv_aasana10(processed_frame,detector)
-            
-                
-                #print(angle, per)
-                #Counting Reps
-            if ex==1:
-                color = (255, 0, 255)
-                if per == 100:
-                    color = (0, 255, 0)
-                    if dir == 0:
-                        count += 0.5
-                        dir = 1
-
-                if per == 0:
-                    color = (0, 255, 0)
-                    if dir == 1:
-                        count += 0.5
-                        dir = 0
-                #cv2.putText(processed_frame, "maintain", (50, 100), cv2.FONT_HERSHEY_PLAIN, 5,(0, 0, 255), 5)
-                #print(count)
-                # Draw Rep Count
-                    cv2.rectangle(processed_frame, (0, 450), (250, 720), (0, 255, 0), cv2.FILLED)
-                    cv2.putText(processed_frame, str(int(count)), (45, 670), cv2.FONT_HERSHEY_PLAIN, 15,
-                                (255, 0, 0), 25)    
-        success,buffer=cv2.imencode('.jpg',processed_frame)
-        frame=buffer.tobytes()
-        yield(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-    cap.release()
+    processed_frame = detector.findPose(processed_frame, False)
+    lmList = detector.findPosition(processed_frame, False)
     
+    if lmList:
+        if aasan == 'ex_1':
+            per = activity.lifting_curls_biceps(processed_frame, detector)
+        elif aasan == 'ex_2':
+            per = activity.lifting_floor_press(processed_frame, detector)
+        elif aasan == 'ex_3':
+            per = activity.lifting_bentover_dumbbell(processed_frame, detector)
+        elif aasan == 'ex_4':
+            per = activity.lifting_forearms(processed_frame, detector)
+        elif aasan == 'ex_5':
+            per = activity.lifting_dumbell_squats(processed_frame, detector)
+        elif aasan == 'ex_6':
+            per = activity.lifting_shoulder_press(processed_frame, detector)
+        elif aasan == 'beg_1':
+            activity.yoga_beg_aasana1(processed_frame, detector)
+        elif aasan == 'beg_2':
+            activity.yoga_beg_aasana2(processed_frame, detector)
+        elif aasan == 'beg_3':
+            activity.yoga_beg_aasana3(processed_frame, detector)
+        elif aasan == 'beg_4':
+            activity.yoga_beg_aasana4(processed_frame, detector)
+        elif aasan == 'beg_5':
+            activity.yoga_beg_aasana5(processed_frame, detector)
+        elif aasan == 'beg_6':
+            activity.yoga_beg_aasana6(processed_frame, detector)
+        elif aasan == 'beg_7':
+            activity.yoga_beg_aasana7(processed_frame, detector)
+        elif aasan == 'beg_8':
+            activity.yoga_beg_aasana8(processed_frame, detector)
+        elif aasan == 'beg_9':
+            activity.yoga_beg_aasana9(processed_frame, detector)
+        elif aasan == 'beg_10':
+            activity.yoga_beg_aasana10(processed_frame, detector)
+        elif aasan == 'inter_1':
+            activity.yoga_inter_aasana1(processed_frame, detector)
+        elif aasan == 'inter_2':
+            activity.yoga_inter_aasana2(processed_frame, detector)
+        elif aasan == 'inter_3':
+            activity.yoga_inter_aasana3(processed_frame, detector)
+        elif aasan == 'inter_4':
+            activity.yoga_inter_aasana4(processed_frame, detector)
+        elif aasan == 'inter_5':
+            activity.yoga_inter_aasana5(processed_frame, detector)
+        elif aasan == 'inter_6':
+            activity.yoga_inter_aasana6(processed_frame, detector)
+        elif aasan == 'inter_7':
+            activity.yoga_inter_aasana7(processed_frame, detector)
+        elif aasan == 'inter_8':
+            activity.yoga_inter_aasana8(processed_frame, detector)
+        elif aasan == 'inter_9':
+            activity.yoga_inter_aasana9(processed_frame, detector)
+        elif aasan == 'inter_10':
+            activity.yoga_inter_aasana10(processed_frame, detector)
+        elif aasan == 'adv_1':
+            activity.yoga_adv_aasana1(processed_frame, detector)
+        elif aasan == 'adv_2':
+            activity.yoga_adv_aasana2(processed_frame, detector)
+        elif aasan == 'adv_3':
+            activity.yoga_adv_aasana3(processed_frame, detector)
+        elif aasan == 'adv_4':
+            activity.yoga_adv_aasana4(processed_frame, detector)
+        elif aasan == 'adv_5':
+            activity.yoga_adv_aasana5(processed_frame, detector)
+        elif aasan == 'adv_6':
+            activity.yoga_adv_aasana6(processed_frame, detector)
+        elif aasan == 'adv_7':
+            activity.yoga_adv_aasana7(processed_frame, detector)
+        elif aasan == 'adv_8':
+            activity.yoga_adv_aasana8(processed_frame, detector)
+        elif aasan == 'adv_9':
+            activity.yoga_adv_aasana9(processed_frame, detector)
+        elif aasan == 'adv_10':
+            activity.yoga_adv_aasana10(processed_frame, detector)
+    
+    return processed_frame
 
 @csrf_exempt
-def video_feed(request,aasan='inter_1'):
-    return StreamingHttpResponse(video_stream(aasan), content_type='multipart/x-mixed-replace; boundary=frame')
-
-# Create your views here.
+def video_feed(request, aasan='inter_1'):
+    if request.method == 'POST':
+        # Read the uploaded frame.
+        frame_file = request.FILES['frame']
+        np_frame = np.frombuffer(frame_file.read(), np.uint8)
+        frame = cv2.imdecode(np_frame, cv2.IMREAD_COLOR)
+        
+        # Process the frame via your video_stream function.
+        processed_frame = video_stream(frame, aasan)
+        
+        # Encode the processed frame as JPEG.
+        success, buffer = cv2.imencode('.jpg', processed_frame)
+        if success:
+            return HttpResponse(buffer.tobytes(), content_type='image/jpeg')
+        else:
+            return HttpResponse(status=500)
+    else:
+        return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
